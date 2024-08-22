2121b3532a51c7d75603c2548c11122244bf4746
Breno Ferreira <165968939+breno-hof@users.noreply.github.com>
2024-08-22 07:05:57 +0000

    include literals and reserved words to scanner

--- src/main/java/br/com/jkoda/Scanner.java
+++ src/main/java/br/com/jkoda/Scanner.java
@@ -1,7 +1,9 @@
 package br.com.jkoda;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static br.com.jkoda.TokenType.*;
 
@@ -11,6 +13,27 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     public Scanner(String source) {
         this.source = source;
@@ -27,34 +50,111 @@ public class Scanner {
     }
 
     private void scanToken() {
-        char c = advance();
-        switch (c) {
-            case ')': addToken(RIGHT_PAREN); break;
-            case '{': addToken(LEFT_BRACE); break;
-            case '}': addToken(RIGHT_BRACE); break;
-            case ',': addToken(COMMA); break;
-            case '.': addToken(DOT); break;
-            case '-': addToken(MINUS); break;
-            case '+': addToken(PLUS); break;
-            case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
-            case '!':
-                addToken(match('=') ? BANG_EQUAL : BANG);
-                break;
-            case '=':
-                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-                break;
-            case '<':
-                addToken(match('=') ? LESS_EQUAL : LESS);
-                break;
-            case '>':
-                addToken(match('=') ? GREATER_EQUAL : GREATER);
-                break;
+        char character = advance();
+        switch (character) {
+            case '(' -> addToken(LEFT_PAREN);
+            case ')' -> addToken(RIGHT_PAREN);
+            case '{' -> addToken(LEFT_BRACE);
+            case '}' -> addToken(RIGHT_BRACE);
+            case ',' -> addToken(COMMA);
+            case '.' -> addToken(DOT);
+            case '-' -> addToken(MINUS);
+            case '+' -> addToken(PLUS);
+            case ';' -> addToken(SEMICOLON);
+            case '*' -> addToken(STAR);
+            case '/' -> addToken(SLASH);
+            case '!' -> addToken(match('=') ? BANG_EQUAL : BANG);
+            case '=' -> addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            case '<' -> addToken(match('=') ? LESS_EQUAL : LESS);
+            case '>' -> addToken(match('=') ? GREATER_EQUAL : GREATER);
+            case '#' -> ignoreComment();
+            case ' ', '\r', '\t' -> {}
+            case '\n' -> line++;
+            case '"' -> readStringLiteral();
+            default -> {
+                if (isDigit(character)) {
+                    readNumberLiteral();
+                    break;
+                }
 
-            default:
+                if (isAlpha(character)) {
+                    readIdentifierLiteral();
+                    break;
+                }
+
                 jKoda.error(line, "Unexpected character.");
-                break;
+            }
         }
+    }
+
+    private boolean isAlpha(char character) {
+        return (character >= 'a' && character <= 'z') ||
+                (character >= 'A' && character <= 'Z') ||
+                character == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void readIdentifierLiteral() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void readNumberLiteral() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char character) {
+        return character >= '0' && character <= '9';
+    }
+
+    private void readStringLiteral() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            jKoda.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private void ignoreComment() {
+        while (peek() != '\n' && !isAtEnd()) advance();
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 
     private boolean match(char expected) {
--- src/test/java/br/com/jkoda/jKodaTest.java
+++ src/test/java/br/com/jkoda/jKodaTest.java
@@ -2,15 +2,22 @@ package br.com.jkoda;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class jKodaTest {
 
     @Test
     void Should_ReadFileLines_When_RunFile() {
+        String[] args = {"src/test/resources/source_code.koda"};
+
+        assertDoesNotThrow(() -> jKoda.main(args));
     }
 
     @Test
     void Should_LogErrorLineAndMessage_When_ReportError() {
+        String[] args = {"src/test/resources/fail_source_code.koda"};
+
+        assertThrows(RuntimeException.class, () -> jKoda.main(args));
     }
 }
